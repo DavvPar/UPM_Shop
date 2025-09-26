925839eb9f10b90cbb62113f16e493b847faeacf
junqin YE <156619255+wol03@users.noreply.github.com>
2025-09-26 13:28:20 +0200

    nuevo repositorio

--- UPM_Shop/src/main/java/es/upm/etsisi/poo/App.java
+++
@@ -1,9 +0,0 @@
-package es.upm.etsisi.poo;
-import java.util.Scanner;
-public class App {
-    public static void main( String[] args ) {
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("HELLO");
-
-    }
-}
