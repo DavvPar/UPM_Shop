a8c876d7fb97d13e3cf89208b0f0713dc9efca11
laurasanzsuarez <laura.sanz.suarez@alumnos.upm.es>
2025-11-12 15:15:28 +0100

    creacion validatePlanningTime

--- src/main/java/es/upm/etsisi/poo/ComplexProduct.java
+++ src/main/java/es/upm/etsisi/poo/ComplexProduct.java
@@ -1,5 +1,7 @@
 package es.upm.etsisi.poo;
 
+import java.time.LocalDate;
+import java.util.Calendar;
 import java.util.Date;
 
 public class ComplexProduct extends Product{
@@ -17,8 +19,35 @@ public class ComplexProduct extends Product{
      */
     public ComplexProduct(int ID, String name, double price, Date expirationDate, int people) {
         super(ID, name, price);
+        if(people <= 0 || people > MAX_PEOPLE)
+            throw new IllegalArgumentException("El numero de participantes debe estar entre 1 y " + MAX_PEOPLE);
+        //TODO REVISAR
+        Date now = new Date();
+        if(expirationDate.before(now))
+            throw new IllegalArgumentException("La fecha de caducidad no puede ser anterior a hoy");
         this.expirationDate = expirationDate;
         this.people = people;
+    }
+
+    private boolean validatePlanningTime(String typeProduct, Date expirationDate){
+        boolean validate = false;
+        Date now = new Date();
+
+        //Diferencia en milisegundos (es como Date guarda los datos)
+        long difms = expirationDate.getTime() - now.getTime();
+
+        //Convertimos a horas
+        long difHours = difms / (1000 * 60 * 60);
+        if(typeProduct.equalsIgnoreCase("addMeeting")){
+            if(difHours >= 12)
+                validate = true;
+        }
+        else if(typeProduct.equalsIgnoreCase("addFood")){
+            //72h = 3 dias
+            if(difHours >= 72)
+                validate = true;
+        }
+        return validate;
     }
 
     public Date getExpirationDate() {
@@ -34,6 +63,8 @@ public class ComplexProduct extends Product{
     }
 
     public void setPeople(int people) {
+        if(people <= 0 || people > MAX_PEOPLE)
+            throw new IllegalArgumentException("El numero de participantes debe estar entre 1 y " + MAX_PEOPLE);
         this.people = people;
     }
 
