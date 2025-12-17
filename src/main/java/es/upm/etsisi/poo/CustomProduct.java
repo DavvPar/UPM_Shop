81fa14340063bdb72733a5ad05f5301999ab2ed4
junqin YE <156619255+wol03@users.noreply.github.com>
2025-12-17 23:29:12 +0100

    Correciones de la Defensa 2 en ticket y getTime y productType en product

--- src/main/java/es/upm/etsisi/poo/CustomProduct.java
+++ src/main/java/es/upm/etsisi/poo/CustomProduct.java
@@ -119,7 +119,7 @@ public class CustomProduct extends Product{
                 }
             }
         }
-        return "{class:"+getProductType() +
+        return "{class:"+super.getProductType() +
                 ", id:"+super.getID() +
                 ", name:'"+super.getName() +
                 "', "+ getCategory().toString() +
